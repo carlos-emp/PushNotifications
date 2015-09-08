1d6d301bc377750afd378c0b52d2d080d82d9329
covazquez <carlos.vazquez@empowerlabs.com>
2015-09-08 11:49:49 -0500

    App

--- js/index.js
+++ js/index.js
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 var app = {
     // Application Constructor
     initialize: function() {
@@ -31,7 +49,7 @@ var app = {
         if (device.platform == 'android' || device.platform == 'Android') {
             alert("Register called");
             //tu Project ID aca!!
-            pushNotification.register(this.successHandler, this.errorHandler,{"senderID":"411517468075","ecb":"app.onNotificationGCM"});
+            pushNotification.register(this.successHandler, this.errorHandler,{"senderID":"PROJECT_ID","ecb":"app.onNotificationGCM"});
         }
         else {
             alert("Register called");
@@ -57,16 +75,16 @@ var app = {
                     document.getElementById('regId').value = e.regid;
                 }
             break;
-
+ 
             case 'message':
               // NOTIFICACION!!!
               alert('message = '+e.message+' msgcnt = '+e.msgcnt);
             break;
-
+ 
             case 'error':
               alert('GCM error = '+e.msg);
             break;
-
+ 
             default:
               alert('An unknown GCM event has occurred');
               break;
@@ -75,7 +93,7 @@ var app = {
     onNotificationAPN: function(event) {
         var pushNotification = window.plugins.pushNotification;
         alert("Running in JS - onNotificationAPN - Received a notification! " + event.alert);
-
+        
         if (event.alert) {
             navigator.notification.alert(event.alert);
         }
